68bca1b8c67a1ad63ced9ff94617d04a6627d5ff
高位强 <985313519@qq.com>
2023-08-15 22:11:55 +0800

    更新：stylelint

--- src/stylelint.config.js
+++ src/stylelint.config.js
@@ -5,5 +5,6 @@ module.exports = {
   rules: {
     'prettier/prettier': true,
     'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['tailwind', 'layer', 'apply', 'config'] }], //兼容tailwindcss
+    'selector-class-pattern': /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$|^el-.*$/, //类名命名为小写，短横向分割， el- 开头的不报错
   },
 }
